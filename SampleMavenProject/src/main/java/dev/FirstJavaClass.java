79f6e5051709a29b947bf0c1f9ae4a4e276a28e9
DELL <DELL@DESKTOP-LFBGPTD>
2024-07-18 02:15:26 +0530

    second commit

--- SampleMavenProject/src/main/java/dev/FirstJavaClass.java
+++ SampleMavenProject/src/main/java/dev/FirstJavaClass.java
@@ -4,7 +4,7 @@ public class FirstJavaClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("First chang to checkin git hub");
+		System.out.println("First change to checkin git hub");
 
 	}
 
